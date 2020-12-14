fde2a502e5d84103310ccb24b5050007a1aafafd
Gary Burgmann <g.burgmann@griffith.edu.au>
2020-12-14 17:06:27 +1000

    update __version__

--- drf_keycloak_auth/__init__.py
+++ drf_keycloak_auth/__init__.py
@@ -3,7 +3,7 @@ __title__ = 'drf_keycloak_auth'
 __description__ = \
     'A convenience libary for authenticating users from Keycloak access tokens'
 __url__ = 'https://gitlab.com/garyburgmann/drf-keycloak-auth'
-__version__ = '0.0.1.dev0'
+__version__ = '0.0.1.dev1'
 __author__ = 'Gary Burgmann'
 __author_email__ = 'garyburgmann@gmail.com'
 __license__ = 'MIT'
