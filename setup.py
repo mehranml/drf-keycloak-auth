8bd6057b127d9672a55e17283d45d64955b381bd
Rob Cosgrove <rob.cosgrove@unimelb.edu.au>
2025-01-14 07:48:13 +1100

    re-instate-pypi

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setuptools.setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.11',
+    python_requires='>=3.10',
     install_requires=[
         'djangorestframework>=3.15,<4',
         'python-keycloak>=5.1',
