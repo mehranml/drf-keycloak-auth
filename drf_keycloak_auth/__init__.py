40bd81419bd43710655ef51c490d458e24d10b07
Rob Cosgrove <rob.cosgrove@unimelb.edu.au>
2025-01-14 07:54:04 +1100

    Update __init__.py

--- drf_keycloak_auth/__init__.py
+++ drf_keycloak_auth/__init__.py
@@ -5,5 +5,5 @@ __description__ = \
 __url__ = 'https://gitlab.com/ecocommons-australia/lib/drf-keycloak-auth'
 __version__ = '0.4.0'
 __author__ = 'EcoCommons Australia'
-__author_email__ = 'ecocommons@griffith.edu.au'
+__author_email__ = 'support@ecocommons.org.au'
 __license__ = 'MIT'
